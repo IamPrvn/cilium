a12050420a4e4b0089f4f609cc96868b8010faf0
Thomas Graf <thomas@cilium.io>
2020-03-16 13:31:26 +0100

    k8s: Disable several CiliumEndpoint status sections by default
    The PATCH'ing of the status field of the CiliumEndpoint is often the bottleneck
    in scalability. However, most of the frequently changing status sections
    provided are not required for operation and only provide additional visibility.
    
    Disable several CiliumEndpoint status sections by default to improve the
    scalability of the CRD-backed identity allocation mode. Introduce a flag
    --endpoint-status to re-enable individual status sections.
    
    The k8sT tests are fixed to use `cilium policy get` instead of relying
    on the policy in the CiliumEndpoint resource. The CEP check in the Kafka
    tests is removed entirely.
    
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -780,6 +780,25 @@ const (
 
 	// K8sHeartbeatTimeout configures the timeout for apiserver heartbeat
 	K8sHeartbeatTimeout = "k8s-heartbeat-timeout"
+
+	// EndpointStatus enables population of information in the
+	// CiliumEndpoint.Status resource
+	EndpointStatus = "endpoint-status"
+
+	// EndpointStatusPolicy enables CiliumEndpoint.Status.Policy
+	EndpointStatusPolicy = "policy"
+
+	// EndpointStatusHealth enables CilliumEndpoint.Status.Health
+	EndpointStatusHealth = "health"
+
+	// EndpointStatusControllers enables CiliumEndpoint.Status.Controllers
+	EndpointStatusControllers = "controllers"
+
+	// EndpointStatusLog enables CiliumEndpoint.Status.Log
+	EndpointStatusLog = "log"
+
+	// EndpointStatusState enables CiliumEndpoint.Status.State
+	EndpointStatusState = "state"
 )
 
 // Default string arguments
@@ -1570,6 +1589,10 @@ type DaemonConfig struct {
 
 	// K8sHeartbeatTimeout configures the timeout for apiserver heartbeat
 	K8sHeartbeatTimeout time.Duration
+
+	// EndpointStatus enables population of information in the
+	// CiliumEndpoint.Status resource
+	EndpointStatus map[string]struct{}
 }
 
 var (
@@ -1586,6 +1609,7 @@ var (
 		EnableIPv4:                   defaults.EnableIPv4,
 		EnableIPv6:                   defaults.EnableIPv6,
 		EnableL7Proxy:                defaults.EnableL7Proxy,
+		EndpointStatus:               make(map[string]struct{}),
 		ENITags:                      make(map[string]string),
 		ToFQDNsMaxIPsPerHost:         defaults.ToFQDNsMaxIPsPerHost,
 		KVstorePeriodicSync:          defaults.KVstorePeriodicSync,
@@ -1703,6 +1727,13 @@ func (c *DaemonConfig) IsFlannelMasterDeviceSet() bool {
 	return len(c.FlannelMasterDevice) != 0
 }
 
+// EndpointStatusIsEnabled returns true if a particular EndpointStatus* feature
+// is enabled
+func (c *DaemonConfig) EndpointStatusIsEnabled(option string) bool {
+	_, ok := c.EndpointStatus[option]
+	return ok
+}
+
 func (c *DaemonConfig) validateIPv6ClusterAllocCIDR() error {
 	ip, cidr, err := net.ParseCIDR(c.IPv6ClusterAllocCIDR)
 	if err != nil {
@@ -1810,6 +1841,13 @@ func (c *DaemonConfig) Validate() error {
 	if c.EnableHostReachableServices && !c.EnableHostServicesUDP && !c.EnableHostServicesTCP {
 		return fmt.Errorf("%s must be at minimum one of [%s,%s]",
 			HostReachableServicesProtos, HostServicesTCP, HostServicesUDP)
+	}
+
+	allowedEndpointStatusValues := EndpointStatusValuesMap()
+	for enabledEndpointStatus := range c.EndpointStatus {
+		if _, ok := allowedEndpointStatusValues[enabledEndpointStatus]; !ok {
+			return fmt.Errorf("unknown endpoint-status option '%s'", enabledEndpointStatus)
+		}
 	}
 
 	return nil
@@ -2172,6 +2210,10 @@ func (c *DaemonConfig) Populate() {
 		c.IPAMAPIBurst = viper.GetInt(IPAMAPIBurst)
 	}
 
+	for _, option := range viper.GetStringSlice(EndpointStatus) {
+		c.EndpointStatus[option] = struct{}{}
+	}
+
 	if c.MonitorQueueSize == 0 {
 		c.MonitorQueueSize = getDefaultMonitorQueueSize(runtime.NumCPU())
 	}
@@ -2410,4 +2452,24 @@ func getDefaultMonitorQueueSize(numCPU int) int {
 		monitorQueueSize = defaults.MonitorQueueSizePerCPUMaximum
 	}
 	return monitorQueueSize
+}
+
+// EndpointStatusValues returns all available EndpointStatus option values
+func EndpointStatusValues() []string {
+	return []string{
+		EndpointStatusPolicy,
+		EndpointStatusHealth,
+		EndpointStatusControllers,
+		EndpointStatusLog,
+		EndpointStatusState,
+	}
+}
+
+// EndpointStatusValuesMap returns all EndpointStatus option values as a map
+func EndpointStatusValuesMap() (values map[string]struct{}) {
+	values = map[string]struct{}{}
+	for _, v := range EndpointStatusValues() {
+		values[v] = struct{}{}
+	}
+	return
 }
